2d3940091a57c5e55c1cda0290bc37dd1cac9580
Anh Pham <anhsmacbook@Anhs-MacBook-Pro.local>
2026-04-17 00:11:43 -0700

    lab3 step2: Automatic current page link

--- global.js
+++ global.js
@@ -3,3 +3,11 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+let navLinks = $$('nav a');
+
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname,
+);
+/* this means only add the class if currentLink exists*/
+currentLink?.classList.add('current');
