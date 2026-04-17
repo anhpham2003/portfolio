85e00e31da313c5c119515e43454632ad7b48564
Anh Pham <anhsmacbook@Anhs-MacBook-Pro.local>
2026-04-17 00:35:26 -0700

    lab3 step3: automatic navigation menu

--- global.js
+++ global.js
@@ -4,10 +4,43 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$('nav a');
+/* handles starting points when the site runs locally vs. from github page*/
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+    ? '/'
+    : '/portfolio/';
 
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname,
-);
-/* this means only add the class if currentLink exists*/
-currentLink?.classList.add('current');
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'resume/', title: 'Resume' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'https://github.com/anhpham2003', title: 'GitHub' },
+];
+
+/* creates nav */
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+
+  /* highlight current page */
+  a.classList.toggle(
+    'current',
+    a.host === location.host && a.pathname === location.pathname,
+  );
+
+  /* open github */
+  if (a.host !== location.host) {
+    a.target = '_blank';
+  }
+
+  nav.append(a);
+}
